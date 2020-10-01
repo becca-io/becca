149910ed6d69e3d0062789ec267bdaaa585fdd60
soso <ethdud1@gmail.com>
2020-10-01 15:59:56 +0900

    chore: add test page

---
+++ apps/homepage/pages/test.tsx
@@ -0,0 +1,13 @@
+import { TextField, Ui } from '@becca/ui';
+import React from 'react';
+
+export const Test = () => {
+  return (
+    <>
+      <Ui></Ui>
+      <TextField />
+    </>
+  );
+};
+
+export default Test;
