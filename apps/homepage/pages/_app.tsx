6e323f823212cdf858a99545028e4cd5ce6db033
soso <ethdud1@gmail.com>
2020-10-01 16:00:10 +0900

    feat: add ThemeProvider

--- apps/homepage/pages/_app.tsx
+++ apps/homepage/pages/_app.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { theme } from '@becca/ui';
+import { ThemeProvider } from 'emotion-theming';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import React from 'react';
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
+import './reset.css';
 import './styles.css';
 
 const CustomApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Welcome to homepage!</title>
       </Head>
@@ -19,7 +22,7 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </main>
       </div>
-    </>
+    </ThemeProvider>
   );
 };
 
